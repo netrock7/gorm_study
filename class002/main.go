5622fca2f72555cf5379a2e90b70dbaa15ce213d
Solo ZHANG <ericzhangmeng@163.com>
2022-04-03 23:02:26 +0800

    2022-04-03 学习记录提交

--- class002/main.go
+++ class002/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 	"time"
 )
 
-var global_db *gorm.DB
+var GLOBAL_DB *gorm.DB
 
 func main() {
-	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:               "go:ha9nD784yO7MFB@tcp(10.38.1.12:3306)/gorm_class?charset=utf8&parseTime=True&loc=Local", // DSN data source name
-		DefaultStringSize: 256,                                                                                       // string 类型字段的默认长度
+	db, _ := gorm.Open(mysql.New(mysql.Config{
+		DSN:               "admin_wr:HjA86jqgm9mZ@tcp(localhost:3306)/gorm_class?charset=utf8&parseTime=True&loc=Local", // DSN data source name
+		DefaultStringSize: 256,                                                                                          // string 类型字段的默认长度
 	}), &gorm.Config{
 		SkipDefaultTransaction: false,
-		NamingStrategy: schema.NamingStrategy{
-			TablePrefix:   "gva_", // 表名前缀，例如表名是user，这里就是t_user
-			SingularTable: false,  //	使用单数表名，user的表名应该是t_users
-		},
+		//NamingStrategy: schema.NamingStrategy{
+		//	TablePrefix:   "gva_", // 表名前缀，例如表名是user，这里就是t_user
+		//	SingularTable: false,  //	使用单数表名，user的表名应该是t_users
+		//},
 		DisableForeignKeyConstraintWhenMigrating: true, //	关闭建立物理外键
 	})
 	//	连接池配置信息
-	sqlDB, err := db.DB()
+	sqlDB, _ := db.DB()
 	sqlDB.SetMaxIdleConns(10)           //	连接池中最大空闲连接数
 	sqlDB.SetMaxOpenConns(100)          //	连接池最大连接数
 	sqlDB.SetConnMaxLifetime(time.Hour) //	连接池中连接的最大可复用时间
-	global_db = db
-	fmt.Println(db, err)
+	GLOBAL_DB = db
+	//fmt.Println(db)
+	TestUserCreate()
 }
--- class002/model.go
+++ class002/model.go
@@ -5,14 +5,25 @@ import (
 	"time"
 )
 
-type User struct {
-	ID           uint
-	Name         string
+type Model struct {
+	UUID uint `gorm:"primaryKey""`
+	Time time.Time
+}
+
+type TestUser struct {
+	//gorm.Model
+	Model Model `gorm:"embedded"`
+	//ID           uint
+	Name         string `gorm:"default:zm"`
 	Email        *string
-	Age          uint8
+	Age          uint8 `gorm:"comment:年龄"` // gorm的标签功能
 	Birthday     *time.Time
 	MemberNumber sql.NullString
 	ActivedAt    sql.NullTime
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	//CreatedAt    time.Time
+	//UpdatedAt    time.Time
+}
+
+func TestUserCreate() {
+	GLOBAL_DB.AutoMigrate(&TestUser{})
 }
